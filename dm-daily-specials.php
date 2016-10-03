af8c73dff3e9b1439c0aee0432627aec21f225d3
Bradford Knowlton <brad@ucanbonline.net>
2016-10-02 22:48:08 -0600

    working short code

---
+++ dm-daily-specials.php
@@ -0,0 +1,222 @@
+<?php
+/*
+Plugin Name: Daily Specials
+Plugin URI: http://rattlesnakemarket.com/
+Description: Check out these daily specials
+Version: 0.1.0
+Author: Bradford Knowlton
+Author URI: http://bradknowlton.com
+*/
+
+function daily_specials( $atts ){
+	
+
+$output = <<<EOF
+<div class="wrap clearfix specials">
+
+        
+    <div class="wpb_column vc_column_container    col-sm-4">
+        <div style="padding-left:0px !important;padding-right:0px !important;" class="vc_column-inner">
+            <div style="position: relative;z-index: 1;" class="wpb_wrapper">
+                
+     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
+        <div class="md-text gizmo-container">
+
+                                            <div class="md-text-title -title "><div>01 Pan Fried Scottish Mackerel</div></div>
+            
+            
+                                        <div class="md-text-content  "><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
+			
+            
+        </div>
+
+    </div>
+    
+     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
+        <div class="md-text gizmo-container">
+
+                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
+            
+            
+                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
+			
+            
+        </div>
+
+    </div>
+   
+     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
+        <div class="md-text gizmo-container">
+
+                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
+            
+            
+                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
+			
+            
+        </div>
+
+    </div>
+   
+                </div>
+        </div>
+    </div>
+    
+    <div class="wpb_column vc_column_container    col-sm-4">
+        <div style="padding-left:0px !important;padding-right:0px !important;" class="vc_column-inner">
+            <div style="position: relative;z-index: 1;" class="wpb_wrapper">
+              
+     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
+        <div class="md-text gizmo-container">
+
+                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
+            
+            
+                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
+			
+            
+        </div>
+
+    </div>
+   
+     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
+        <div class="md-text gizmo-container">
+
+                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
+            
+            
+                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
+			
+            
+        </div>
+
+    </div>
+   
+     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
+        <div class="md-text gizmo-container">
+
+                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
+            
+            
+                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
+			
+            
+        </div>
+
+    </div>
+  
+                </div>
+        </div>
+    </div>
+    
+    <div class="wpb_column vc_column_container    col-sm-4">
+        <div style="padding-left:0px !important;padding-right:0px !important;" class="vc_column-inner">
+            <div style="position: relative;z-index: 1;" class="wpb_wrapper">
+              
+     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
+        <div class="md-text gizmo-container">
+
+                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
+            
+            
+                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
+			
+            
+        </div>
+
+    </div>
+   
+     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
+        <div class="md-text gizmo-container">
+
+                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
+            
+            
+                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
+			
+            
+        </div>
+
+    </div>
+    
+      <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
+        <div class="md-text gizmo-container">
+
+                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
+            
+            
+                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
+			
+            
+        </div>
+
+    </div>
+  
+       
+    
+
+                </div>
+        </div>
+    </div>
+    
+    
+
+    </div>
+    
+     <style scoped="">
+
+        /* Solid Style*/
+                    .specials .md-text-title {
+                color: rgb(255, 255, 255);
+            }
+        
+    .specials{
+        text-align:     center;
+
+    }
+    .specials .md-text-title{
+            font-size:      20px;
+            line-height:    20px;
+            letter-spacing: 0px;
+            margin-bottom: 12px;
+            transition: all .3s cubic-bezier(0.215, 0.61, 0.355, 1) ;
+                        font-family:    Playfair Display;
+            font-style:     bold;
+            font-weight:    700;
+                    }
+    .specials .md-text-title:not(.title-slider):hover{
+        letter-spacing:  0px;
+    }
+    .specials .md-text-title-separator{
+            margin-bottom:10px ;
+            width: 110px;
+            border-top: 5px solid rgb(0, 255, 153);
+                    margin-left: auto;
+            margin-right: auto;
+                }
+
+    .specials .md-text-content{
+     margin-bottom: 24px;
+    }
+
+    .specials .md-text-content p{
+            color:          rgb(255, 255, 255);
+            font-size:      16px;
+            line-height:    25px;
+                    font-family:    Roboto;
+            font-style:     regular;
+            font-weight:    400;
+            }
+
+                .specials .md-text-content p{
+            margin: 0  auto;
+        }
+        
+
+    </style>
+EOF;
+
+return $output;
+
+}
+add_shortcode( 'daily-specials', 'daily_specials' );
