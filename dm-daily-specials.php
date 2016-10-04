afe4951cbdadc2ac5c2f4a680ac2fa51ef595a5e
Bradford Knowlton <brad@ucanbonline.net>
2016-10-03 19:20:57 -0600

    Working version with ACF

--- dm-daily-specials.php
+++ dm-daily-specials.php
@@ -8,156 +8,115 @@ Author: Bradford Knowlton
 Author URI: http://bradknowlton.com
 */
 
-function daily_specials( $atts ){
-	
-
-$output = <<<EOF
-<div class="wrap clearfix specials">
-
+add_action( 'init', 'register_cpt_special' );
+function register_cpt_special() {
+    $labels = array( 
+        'name' => _x( 'Specials', 'special' ),
+        'singular_name' => _x( 'Special', 'special' ),
+        'add_new' => _x( 'Add New', 'special' ),
+        'add_new_item' => _x( 'Add New Special', 'special' ),
+        'edit_item' => _x( 'Edit Special', 'special' ),
+        'new_item' => _x( 'New Special', 'special' ),
+        'view_item' => _x( 'View Special', 'special' ),
+        'search_items' => _x( 'Search Specials', 'special' ),
+        'not_found' => _x( 'No specials found', 'special' ),
+        'not_found_in_trash' => _x( 'No specials found in Trash', 'special' ),
+        'parent_item_colon' => _x( 'Parent Special:', 'special' ),
+        'menu_name' => _x( 'Specials', 'special' ),
+    );
+    $args = array( 
+        'labels' => $labels,
+        'hierarchical' => false,
         
-    <div class="wpb_column vc_column_container    col-sm-4">
-        <div style="padding-left:0px !important;padding-right:0px !important;" class="vc_column-inner">
-            <div style="position: relative;z-index: 1;" class="wpb_wrapper">
+        'supports' => array( 'title',), //  'editor', 'thumbnail'
+        'public' => false,
+        'show_ui' => true,
+        'show_in_menu' => true,
+        
+        'menu_icon' => 'dashicons-carrot',
+        
+        'show_in_nav_menus' => false,
+        'publicly_queryable' => false,
+        'exclude_from_search' => true,
+        'has_archive' => false,
+        'query_var' => false,
+        'can_export' => true,
+        'rewrite' => false,
+        'capability_type' => 'post'
+    );
+    register_post_type( 'specials', $args );
+}
+
+function daily_specials( $atts ){
+
+
+$output .= '
+<div class="wrap clearfix specials">';
                 
-     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
-        <div class="md-text gizmo-container">
+$today = getdate();
+$today_args = array(
+    'year' => $today['year'],
+    'monthnum' => $today['mon'],
+    'day' => $today['mday'],
+    'post_type' => 'specials' 
+    // possibly further query arguments
+);
 
-                                            <div class="md-text-title -title "><div>01 Pan Fried Scottish Mackerel</div></div>
-            
-            
-                                        <div class="md-text-content  "><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
-			
-            
-        </div>
+$today_query = new WP_Query( $today_args );
 
-    </div>
+if ( $today_query->have_posts() ) {
+
+while ( $today_query->have_posts() ) :
+    $today_query->the_post();
+    // echo something
     
-     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
-        <div class="md-text gizmo-container">
-
-                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
-            
-            
-                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
-			
-            
-        </div>
-
-    </div>
-   
-     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
-        <div class="md-text gizmo-container">
-
-                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
-            
-            
-                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
-			
-            
-        </div>
-
-    </div>
-   
-                </div>
-        </div>
-    </div>
-    
-    <div class="wpb_column vc_column_container    col-sm-4">
+    $output .= ' 
+    	<div class="wpb_column vc_column_container col-sm-4" style="min-height: 185px;">
         <div style="padding-left:0px !important;padding-right:0px !important;" class="vc_column-inner">
             <div style="position: relative;z-index: 1;" class="wpb_wrapper">
-              
-     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
+
+    <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
         <div class="md-text gizmo-container">
-
-                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
-            
-            
-                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
-			
-            
-        </div>
-
-    </div>
-   
-     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
-        <div class="md-text gizmo-container">
-
-                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
-            
-            
-                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
-			
-            
-        </div>
-
-    </div>
-   
-     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
-        <div class="md-text gizmo-container">
-
-                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
-            
-            
-                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
-			
-            
-        </div>
-
-    </div>
-  
-                </div>
-        </div>
+			<div class="md-text-title -title ">'.get_the_title().'</div>
+                   <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">'.get_field( 'special_line_1' ).'</p><p style="text-align: center; opacity: 0.7;">'.get_field( 'special_line_2').'</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>'.get_field( 'special_price').'</strong></span></p></div>
+	    </div>
     </div>
     
-    <div class="wpb_column vc_column_container    col-sm-4">
+     </div>
+        </div>
+    </div>
+';
+    
+    
+endwhile;
+
+     
+} else {
+	// no posts found
+	
+$output .= ' 
+    	<div class="wpb_column vc_column_container col-sm-12" style="min-height: 150px;">
         <div style="padding-left:0px !important;padding-right:0px !important;" class="vc_column-inner">
             <div style="position: relative;z-index: 1;" class="wpb_wrapper">
-              
-     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
+
+    <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
         <div class="md-text gizmo-container">
-
-                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
-            
-            
-                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
-			
-            
-        </div>
-
-    </div>
-   
-     <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
-        <div class="md-text gizmo-container">
-
-                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
-            
-            
-                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
-			
-            
-        </div>
-
+			<div class="md-text-title -title ">No Specials Posted Yet</div>
+	    </div>
     </div>
     
-      <div class="md-text-container  md-align-center wpb_wrapper wpb_md_text_wrapper ui-md_text">
-        <div class="md-text gizmo-container">
-
-                                            <div class="md-text-title -title ">01 Pan Fried Scottish Mackerel</div>
-            
-            
-                                        <div class="md-text-content  "><p></p><p style="text-align: center; opacity: 0.7;">Mint parsley with apple cider vinegar, salt, spices</p><p data-mce-placeholder="1" data-wp-more-text="" data-wp-more="more" title="Read more..." style="text-align: center;" class="wp-more-tag mce-wp-more"><span style="color: #c4b37c;"><strong>200g &mdash; $10.00</strong></span></p></div>
-			
-            
+     </div>
         </div>
-
     </div>
-  
-       
+';
+	
+	
+}       
+
+wp_reset_postdata();
     
-
-                </div>
-        </div>
-    </div>
+$output .= '
+               
     
     
 
@@ -214,7 +173,7 @@ $output = <<<EOF
         
 
     </style>
-EOF;
+';
 
 return $output;
 
